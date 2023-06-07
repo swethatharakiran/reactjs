46b5f7a8aa3dc8e0b8aa5b294434999ea7667c0b
swethatharakiran <115216446+swethatharakiran@users.noreply.github.com>
2023-06-07 12:20:31 +0530

    passing data via props
    1)sending data from one component to another.
    2)using loops in jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
+    const expenses=[{title:'car insurance',amount:5000,date:new Date(2023,5,7),location:'banglore'},
+      {title:'new TV',amount:50000,date:new Date(2023,4,7),location:'tumkur'},
+      {title:'Desk',amount:8000,date:new Date(2023,5,5),location:'mysore'},]
+    
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
-    </div>
+        {expenses.map((item)=>{
+          return (
+            <ExpenseItem title={item.title} 
+            amount={item.amount} 
+            date={item.date}
+            location={item.location}></ExpenseItem>
+          )
+        })}
+      </div>
+   
   );
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,14 @@
 import './ExpenseItem.css'
-function ExpenseItem(){
-    const expensedate=new Date(2023, 5, 7);
-    const expensetitle='Car Insurance'
-    const expenseamount=5000
-    const locationOfExpenditure='banglore'
+function ExpenseItem(props){
+    
     return (
         <div className='expense-item'>
-        <div>{expensedate.toISOString()}</div>
+        <div>{props.date.toISOString()}</div>
         <div className='expense-item__description'>
-        <h2>{expensetitle}</h2>
-        <div className='expense-item__price'>Rs. {expenseamount} </div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>Rs. {props.amount} </div>
         </div>
-        <h2>{locationOfExpenditure}</h2>
+        <h2>{props.location}</h2>
          </div>
         );
 }
