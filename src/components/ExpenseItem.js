ce5a8ac9d68f73e83b04f3d69856266a54a98f7c
swethatharakiran <115216446+swethatharakiran@users.noreply.github.com>
2023-06-07 09:52:50 +0530

    sample reactjs app -first app

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return (
+        <div>
+        <h2>Expense Items</h2>
+        <h3>food 10</h3>
+        <h3>petrol 100</h3>
+        <h3>movies 200</h3> </div>
+        );
+}
+
+export default ExpenseItem
