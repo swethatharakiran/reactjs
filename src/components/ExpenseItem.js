a2bd4017f24b3ed8e5dea13b0e68e093bd93db92
swethatharakiran <115216446+swethatharakiran@users.noreply.github.com>
2023-06-07 10:42:21 +0530

    js variables in jsx

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,18 @@
+import './ExpenseItem.css'
 function ExpenseItem(){
+    const expensedate=new Date(2023, 5, 7);
+    const expensetitle='Car Insurance'
+    const expenseamount=5000
+    const locationOfExpenditure='banglore'
     return (
-        <div>
-        <h2>Expense Items</h2>
-        <h3>food 10</h3>
-        <h3>petrol 100</h3>
-        <h3>movies 200</h3> </div>
+        <div className='expense-item'>
+        <div>{expensedate.toISOString()}</div>
+        <div className='expense-item__description'>
+        <h2>{expensetitle}</h2>
+        <div className='expense-item__price'>Rs. {expenseamount} </div>
+        </div>
+        <h2>{locationOfExpenditure}</h2>
+         </div>
         );
 }
 
