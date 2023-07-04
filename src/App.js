80ea3d4562c84a8825abef484dbe4e8702bdd707
swethatharakiran <115216446+swethatharakiran@users.noreply.github.com>
2023-07-03 22:05:00 -0700

    making expense form input visible on screen

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import ExpenseForm from './components/Expenses/ExpenseForm.js';
+//import ExpenseForm from './components/NewExpense/ExpenseForm.js';
 import Card from './components/UI/Card.js'
 import ExpenseItem from "./components/Expenses/ExpenseItem";
+import NewExpense from './components/NewExpense/NewExpense.js';
+import React,{useState} from 'react';
 //import ExpenseDate from "./components/ExpenseDate";
 function App() {
+
+    
     const expenses=[{title:'car insurance',amount:5000,date:new Date(2023,5,7),location:'banglore'},
       {title:'new TV',amount:50000,date:new Date(2023,4,7),location:'tumkur'},
       {title:'Desk',amount:8000,date:new Date(2023,5,5),location:'mysore'},]
     
+      const [myExpenses,setMyExpense]=useState(expenses);
+
+      const addExpenseHandler=(expenseData)=>{
+        console.log('frm app.js file')
+        console.log(expenseData);
+        const updatedExpenses=[...myExpenses,
+          {'title': expenseData.title,'amount': expenseData.amount,'date': new Date(expenseData.date)}]
+        
+        setMyExpense(updatedExpenses);
+      }
+    console.log(expenses);
+
+
   return (
     <Card>
       
-      <ExpenseForm></ExpenseForm>
+      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
       
         
-        {expenses.map((item)=>{
+        {myExpenses.map((item)=>{
           return (
           
             <ExpenseItem title={item.title} 
